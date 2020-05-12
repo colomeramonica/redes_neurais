2ecfae7de33b03a3494d80dbef51694fdd655702
Mônica Colomera <colomeramonica@gmail.com>
2020-05-11 21:28:33 -0300

    Criação da Rede, que vai ser a main do projeto

---
+++ rede.py
@@ -0,0 +1,29 @@
+from mcp import MCP
+from neuronio import Neuronio
+
+class Rede_Neural:
+
+    def __init__(self, entradas, res_esperado, pesos_h, pesos_o):
+        self.entradas = entradas
+        self.res_esperado = res_esperado
+        self.pesos_h = pesos_h
+        self.pesos_o = pesos_o
+        self.taxa_aprendizado = 0.2
+        self.h1 = []
+        self.h2 = []
+        self.delta_h1 = [0, 0, 0]
+        self.delta_h2 = [0, 0, 0]
+        self.delta_o1 = [0, 0, 0]
+        self.delta_o2 = [0, 0, 0]
+
+
+    def getMSE(self):
+        for (idx, val) in range(self.entradas):
+            neuronio = Neuronio(val, self.pesos_h[0])
+            self.h1 = neuronio.saida()
+
+            neuronio = Neuronio(val, self.pesos_h[1])
+            self.h2 = neuronio.saida()
+
+            u1 = self.pesos_o[0][0] + self.h1[idx] * self.pesos_o[0][1] + self.h2[idx] * self.pesos_o[0][2]
+            u2 = self.pesos_o[0][0] + self.h1[idx] * self.pesos_o[1][1] + self.h2[idx] * self.pesos_o[1][2]
